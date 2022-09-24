d2834a73912cdd84c9324f8f4e94dbaf5fc1fea2
Jason K. Moore <moorepants@gmail.com>
2022-09-24 13:03:23 +0200

    Full URL for news menu.

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,7 +32,7 @@ PAGE_URL = '{slug}.html'
 PAGE_SAVE_AS = '{slug}.html'
 PAGE_ORDER_BY = 'sortorder'
 
-MENUITEMS = [('News', 'news/')]
+MENUITEMS = [('News', 'http://2023.bmdconf.org/news/')]
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
